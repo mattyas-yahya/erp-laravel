68220add84a5f1b52f58d2ff5483f9f95df391be
Alfan Zain <alfanzainkp@gmail.com>
2023-12-05 11:13:41 +0700

    revert 1 bulan yang lalu soale ora bayaran

--- resources/views/tms/delivery-schedule/_components/header-table.blade.php
+++
@@ -1,8 +0,0 @@
-<tr>
-    <th>{{ __('Vehicle') }}</th>
-    <th>{{ __('Driver') }}</th>
-    <th>{{ __('Start Date') }}</th>
-    <th>{{ __('Starting Odometer') }}</th>
-    <th>{{ __('Comment') }}</th>
-    <th>{{ __('Action') }}</th>
-</tr>
--- resources/views/tms/vehicle/_components/header-table.blade.php
+++
@@ -1,8 +0,0 @@
-<tr>
-    <th>{{ __('Plat Nomer') }}</th>
-    <th>{{ __('Branch') }}</th>
-    <th>{{ __('No. Lambung') }}</th>
-    <th>{{ __('Pemilik') }}</th>
-    <th>{{ __('Active') }}</th>
-    <th>{{ __('Action') }}</th>
-</tr>
